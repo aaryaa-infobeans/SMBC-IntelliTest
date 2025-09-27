ed4c72550f9494b88b7ab18302b76174d9f851ea
aaryaa-infobeans <aaryaa-infobeans@users.noreply.github.com>
2025-09-27 16:56:58 +0000

    deploy: 533b59aa61a612bf4f5733ade09476a4e25d0510

--- SRC/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from dotenv import load_dotenv
-
-load_dotenv()  # This loads the .env file
-# from config import XrayConfig
--- SRC/tests/tests_ui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Common imports and setup for UI tests module.
-"""
-
-from Utilities.ReportUtils.logger import get_logger
-
-# Common logger instance for all UI tests
-logger = get_logger()
